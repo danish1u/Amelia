b97507c1172453bae7805d32d6f15d63b3b6dbf6
danish1u <danishprince786a@gmail.com>
2019-05-13 23:04:31 +0500

    Update and rename placeholder23.js to placeholder24.js

--- placeholder24.js
+++ placeholder24.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#v4-1:first").attr("placeholder", "&#xF002;");  
+  $(".v4sbox:first").attr("placeholder", "&#xF002;");  
 });
 $(document).ready(function() {
   $(".v4sbox").attr("placeholder", "Search");  
