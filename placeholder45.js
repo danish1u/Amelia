01058e36cf4ab98c6b5f146ed35ed2939aa9939c
danish1u <danishprince786a@gmail.com>
2019-05-18 00:52:08 +0500

    Update and rename placeholder44.js to placeholder45.js

--- placeholder45.js
+++ placeholder45.js
@@ -3,4 +3,6 @@ $(document).ready(function()
                  {
   $(".v4sbox").attr("placeholder", "&#xe003; Search");  
 });
+$(document).ready(function() {
 $('.v4sbox').addClass('glyphicon');
+});
