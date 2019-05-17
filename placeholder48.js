f7459c587a5fec202ee253237a91bd4628e1bb93
danish1u <danishprince786a@gmail.com>
2019-05-18 01:10:51 +0500

    Update and rename placeholder47.js to placeholder48.js

--- placeholder48.js
+++ placeholder48.js
@@ -3,6 +3,8 @@ $(document).ready(function()
                  {
   $(".v4sbox").attr("placeholder", " Search");  
 });
+
 $(document).ready(function() {
-$( ".v4mid td:nth-child(2)" ).append( " <i class="icon-circle-arrow-right icon-large"></i>" );
+  $( " <i class="icon-circle-arrow-right icon-large"></i>" ).prependTo( ".v4mid td:odd" );
+  
 });
